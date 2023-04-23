a071b72879a8ba0d797c1d48014517ffd09a8228
iDebonair <ofemiowolabi@gmail.com>
2023-04-23 23:47:01 +0000

    Implemented assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = (actual, expected) => { 
+  for(let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]){
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)){
+    console.log (`Assertion Passed: ${actual} === ${expected}`)
+  } else{
+    console.log (`Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+assertArrayEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArrayEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArrayEqual(["1", "2", "3"], ["1", "2", 3]); // => false
