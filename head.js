739d6bf3088f26840ccfcc0bd0e129f4f2201463
iDebonair <ofemiowolabi@gmail.com>
2023-05-10 11:23:24 +0000

    Refactored to include the export module

--- head.js
+++ head.js
@@ -3,7 +3,4 @@ function head(list){
   return list[0];
 }
 
-
-const assertEqual = require('./assertEqual');
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
