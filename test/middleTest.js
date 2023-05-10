45839963ddb7c61bdd8efb63510fe425c7168464
iDebonair <ofemiowolabi@gmail.com>
2023-05-10 11:56:54 +0000

    Refactored to include to include mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,9 @@
+const assert = require('chai').assert;
+
 const middle = require('../middle');
 
-console.log (middle([1, 2, 3, 4, 5, 6]))
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,11 @@
 const assert = require('chai').assert;
-const { strictEqual } = require('assert');
-const assertEqual = require('../assertEqual');
-
 const tail = require('../tail');
 
 describe("#tail", () => {
-  it("returns '3' for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual (assertEqual(['Yo Yo', 'Lighthouse', 'Labs'], 3))
-  })
-})
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const input = ['Yo Yo', 'Lighthouse', 'Labs'];
+    const expectedOutput = ['Lighthouse', 'Labs'];
+    assert.deepEqual(tail(input), expectedOutput);
+  });
+});
 
