6227ec96b0af569393c9745e66ec22f96dca885a
iDebonair <ofemiowolabi@gmail.com>
2023-05-10 11:13:54 +0000

    Refactored to include the require function

--- middle.js
+++ middle.js
@@ -1,27 +1,12 @@
-const eqArrays = (actual, expected) => { 
-  if (actual.length !== expected.length){
-    return false;
-  }
-  for(let i = 0; i < actual.length; i++) {
-    if(actual[i] !== expected[i])
-    return false;
-};
-return true;
-};
-
-
-const assertArrayEqual = (actual, expected) => {
-if (eqArrays(actual, expected)){
-  console.log (`Assertion Passed: ${actual} === ${expected}`)
-} else{
-  console.log (`Assertion Failed: ${actual} !== ${expected}`)
-}
-}
-assertArrayEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArrayEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-
+const eqArrays = require('./eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const assertArrayEqual = (actual, expected) => {
+// if (eqArrays(actual, expected)){
+//   console.log (`Assertion Passed: ${actual} === ${expected}`)
+// } else{
+//   console.log (`Assertion Failed: ${actual} !== ${expected}`)
+// }
+// }
 const middle = (arr) => {
   const newArrayLength = arr.length;
   if (newArrayLength <= 2) {
@@ -33,4 +18,5 @@ const middle = (arr) => {
     return [arr[Math.floor(newArrayLength / 2)]];
   }
 };
-console.log (middle([1, 2, 3, 4, 5, 6]))
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,3 @@
+const middle = require('../middle');
+
+console.log (middle([1, 2, 3, 4, 5, 6]))
