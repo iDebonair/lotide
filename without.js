821e2c158cc00444277117f9c20e511c4e9ec196
iDebonair <ofemiowolabi@gmail.com>
2023-05-16 03:51:07 +0000

    New commit based on code review

--- without.js
+++
@@ -1,9 +0,0 @@
-const eqArrays = require('./eqArrays');
-
-const assertArraysEqual = require('./assertArraysEqual');
-
-const without = (actual, expected) => {
-  return actual.filter(arr => !expected.includes(arr));
-};
-module.exports = without;
-
